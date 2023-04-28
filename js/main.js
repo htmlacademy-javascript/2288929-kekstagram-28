f72eadbc4bf76a3c75ff5739861582339e21f6b0
olissotib <olissotib@gmail.com>
2023-04-28 10:58:48 +0300

    Добавил модуль валидации формы, сделал часть задания 1-5

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import {createPhotos} from './data.js';
 import {renderPreviews} from './preview.js';
+import './valid-img-form.js';
 
 const usersPhotos = createPhotos();
 renderPreviews(usersPhotos);
