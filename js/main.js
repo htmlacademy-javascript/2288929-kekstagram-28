bcf9a5d72d59cec097f505e1954a1e1bddff4bb5
olissotib <olissotib@gmail.com>
2023-05-25 20:08:44 +0300

    Внес испревления

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import {createPhotos} from './data.js';
 import {renderPreviews} from './preview.js';
-import './valid-img-form.js';
+import './form-validation.js';
 
 const usersPhotos = createPhotos();
 renderPreviews(usersPhotos);
